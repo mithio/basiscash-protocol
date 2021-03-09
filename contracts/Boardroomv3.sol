fbdc82383d7fd0a303b6d648e4bcabcecb2f7f40
eepdev <78463658+eepdev@users.noreply.github.com>
2021-03-09 17:57:50 -0500

    Add Boardroomv3
    The bucket system from v2 was removed (may be reinstated at a later date)
    Boardroomv3 has a 5 day withdrawal delay

---
+++ contracts/Boardroomv3.sol
@@ -0,0 +1,276 @@
+pragma solidity ^0.6.0;
+//pragma experimental ABIEncoderV2;
+
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import '@openzeppelin/contracts/token/ERC20/SafeERC20.sol';
+
+import './lib/Safe112.sol';
+import './owner/Operator.sol';
+import './utils/ContractGuard.sol';
+import './interfaces/IBasisAsset.sol';
+import './interfaces/IFeeDistributorRecipient.sol';
+
+contract ShareWrapper {
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+
+    IERC20 public share;
+
+    uint256 private _totalSupply;
+    mapping(address => uint256) private _balances;
+
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) public view returns (uint256) {
+        return _balances[account];
+    }
+
+    function stake(uint256 amount) public virtual {
+        _totalSupply = _totalSupply.add(amount);
+        _balances[msg.sender] = _balances[msg.sender].add(amount);
+        share.safeTransferFrom(msg.sender, address(this), amount);
+    }
+
+    function withdraw(uint256 amount) public virtual {
+        uint256 directorShare = _balances[msg.sender];
+        require(
+            directorShare >= amount,
+            'Boardroom: withdraw request greater than staked amount'
+        );
+        _totalSupply = _totalSupply.sub(amount);
+        _balances[msg.sender] = directorShare.sub(amount);
+        share.safeTransfer(msg.sender, amount);
+    }
+}
+
+contract Boardroomv3 is ShareWrapper, ContractGuard, Operator, IFeeDistributorRecipient {
+    using SafeERC20 for IERC20;
+    using Address for address;
+    using SafeMath for uint256;
+    using Safe112 for uint112;
+
+    /* ========== DATA STRUCTURES ========== */
+
+    struct Boardseat {
+        uint256 lastSnapshotIndex;
+        uint256 rewardEarned;
+        uint256 pendingWithdrawalBalance;
+        uint256 pendingWithdrawalTime;
+    }
+
+    struct BoardSnapshot {
+        uint256 time;
+        uint256 rewardReceived;
+        uint256 rewardPerShare;
+    }
+
+    /* ========== STATE VARIABLES ========== */
+
+    IERC20 private cash;
+
+    mapping(address => Boardseat) private directors;
+    BoardSnapshot[] private boardHistory;
+
+    /* ========== CONSTRUCTOR ========== */
+
+    constructor(IERC20 _cash, IERC20 _share) public {
+        cash = _cash;
+        share = _share;
+
+        BoardSnapshot memory genesisSnapshot = BoardSnapshot({
+            time: block.number,
+            rewardReceived: 0,
+            rewardPerShare: 0
+        });
+        boardHistory.push(genesisSnapshot);
+    }
+
+    /* ========== Modifiers =============== */
+    modifier directorExists {
+        require(
+            balanceOf(msg.sender) > 0,
+            'Boardroom: The director does not exist'
+        );
+        _;
+    }
+
+    modifier updateReward(address director) {
+        if (director != address(0)) {
+            Boardseat memory seat = directors[director];
+            seat.rewardEarned = earned(director);
+            seat.lastSnapshotIndex = latestSnapshotIndex();
+            directors[director] = seat;
+        }
+        _;
+    }
+
+    /* ========== VIEW FUNCTIONS ========== */
+
+    // =========== Snapshot getters
+
+    function latestSnapshotIndex() public view returns (uint256) {
+        return boardHistory.length.sub(1);
+    }
+
+    function getLatestSnapshot() internal view returns (BoardSnapshot memory) {
+        return boardHistory[latestSnapshotIndex()];
+    }
+
+    function getLastSnapshotIndexOf(address director)
+        public
+        view
+        returns (uint256)
+    {
+        return directors[director].lastSnapshotIndex;
+    }
+
+    function getLastSnapshotOf(address director)
+        internal
+        view
+        returns (BoardSnapshot memory)
+    {
+        return boardHistory[getLastSnapshotIndexOf(director)];
+    }
+
+    // =========== Director getters
+
+    function rewardPerShare() public view returns (uint256) {
+        return getLatestSnapshot().rewardPerShare;
+    }
+
+    function earned(address director) public view returns (uint256) {
+        uint256 latestRPS = getLatestSnapshot().rewardPerShare;
+        uint256 storedRPS = getLastSnapshotOf(director).rewardPerShare;
+
+        return
+            balanceOf(director).mul(latestRPS.sub(storedRPS)).div(1e18).add(
+                directors[director].rewardEarned
+            );
+    }
+
+    function pendingWithdrawalBalance(address director) public view returns (uint256) {
+        return directors[director].pendingWithdrawalBalance;
+    }
+
+    function pendingWithdrawalTime(address director) public view returns (uint256) {
+        return directors[director].pendingWithdrawalTime;
+    }
+
+    /* ========== MUTATIVE FUNCTIONS ========== */
+
+    function stake(uint256 amount)
+        public
+        override
+        onlyOneBlock
+        updateReward(msg.sender)
+    {
+        require(amount > 0, 'Boardroom: Cannot stake 0');
+        super.stake(amount);
+        emit Staked(msg.sender, amount);
+    }
+
+    function withdraw(uint256 amount)
+        public
+        override
+        onlyOneBlock
+        directorExists
+        updateReward(msg.sender)
+    {
+        require(amount > 0, 'Boardroom: Cannot withdraw 0');
+        super.withdraw(amount);
+        emit Withdrawn(msg.sender, amount);
+    }
+
+    function exit() external {
+        withdraw(balanceOf(msg.sender));
+        claimReward();
+    }
+
+    function claimPendingReward() public {
+        uint256 timeCanWithdraw = directors[msg.sender].pendingWithdrawalTime;
+        require(timeCanWithdraw <= now, 'Boardroom: Cannot claim pending reward yet');
+
+        uint256 reward = directors[msg.sender].pendingWithdrawalBalance;
+
+        if (reward > 0) {
+            directors[msg.sender].pendingWithdrawalBalance = 0;
+            directors[msg.sender].pendingWithdrawalTime = 0;
+
+            cash.safeTransfer(msg.sender, reward);
+            emit RewardPaid(msg.sender, reward);
+        }
+    }
+
+    function claimReward() public updateReward(msg.sender) {
+        uint256 reward = directors[msg.sender].rewardEarned;
+        if (reward > 0) {
+            uint256 newPendingWithdrawalBalance = directors[msg.sender].pendingWithdrawalBalance.add(reward);
+            directors[msg.sender].rewardEarned = 0;
+            directors[msg.sender].pendingWithdrawalBalance = newPendingWithdrawalBalance;
+            directors[msg.sender].pendingWithdrawalTime = now + 5 days;
+            emit RewardPending(msg.sender, reward);
+        }
+    }
+
+    function allocateSeigniorage(uint256 amount)
+        external
+        onlyOneBlock
+        onlyOperator
+    {
+        require(amount > 0, 'Boardroom: Cannot allocate 0');
+        require(
+            totalSupply() > 0,
+            'Boardroom: Cannot allocate when totalSupply is 0'
+        );
+
+        // Create & add new snapshot
+        uint256 prevRPS = getLatestSnapshot().rewardPerShare;
+        uint256 nextRPS = prevRPS.add(amount.mul(1e18).div(totalSupply()));
+
+        BoardSnapshot memory newSnapshot = BoardSnapshot({
+            time: block.number,
+            rewardReceived: amount,
+            rewardPerShare: nextRPS
+        });
+        boardHistory.push(newSnapshot);
+
+        cash.safeTransferFrom(msg.sender, address(this), amount);
+        emit RewardAdded(msg.sender, amount);
+    }
+
+    function allocateTaxes(uint256 amount)
+        external
+        onlyOneBlock
+        onlyFeeDistributor
+    {
+        require(amount > 0, 'Boardroom: Cannot allocate 0');
+        require(
+            totalSupply() > 0,
+            'Boardroom: Cannot allocate when totalSupply is 0'
+        );
+
+        // Create & add new snapshot
+        uint256 prevRPS = getLatestSnapshot().rewardPerShare;
+        uint256 nextRPS = prevRPS.add(amount.mul(1e18).div(totalSupply()));
+
+        BoardSnapshot memory newSnapshot = BoardSnapshot({
+            time: block.number,
+            rewardReceived: amount,
+            rewardPerShare: nextRPS
+        });
+        boardHistory.push(newSnapshot);
+
+        cash.safeTransferFrom(msg.sender, address(this), amount);
+        emit RewardAdded(msg.sender, amount);
+    }
+
+    /* ========== EVENTS ========== */
+
+    event Staked(address indexed user, uint256 amount);
+    event Withdrawn(address indexed user, uint256 amount);
+    event RewardPaid(address indexed user, uint256 reward);
+    event RewardAdded(address indexed user, uint256 reward);
+    event RewardPending(address indexed user, uint256 reward);
+}
