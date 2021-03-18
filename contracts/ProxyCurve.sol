1304574fcb15b8164debce463b1f72b08218b791
eepdev <78463658+eepdev@users.noreply.github.com>
2021-03-18 18:03:18 -0400

    fix arguments
    need to use int128 instead of uint128

--- contracts/ProxyCurve.sol
+++ contracts/ProxyCurve.sol
@@ -3,9 +3,8 @@ pragma solidity ^0.6.2;
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import '@openzeppelin/contracts/token/ERC20/SafeERC20.sol';
 import '@openzeppelin/contracts/math/SafeMath.sol';
-import "./interfaces/ICurveMeta2.sol";
 import './owner/Operator.sol';
-import './Cashv2.sol';
+import "./interfaces/ICurveMeta2.sol";
 import './interfaces/IFeeChecker.sol';
 import './interfaces/IFeeDistributor.sol';
 
@@ -30,7 +29,7 @@ contract ProxyCurve is Operator {
     /// @param dx Amount of MIC being exchanged
     /// @param min_dy Minimum amount of `j` to receive, should be calculated on the website based on `transferToAmount` (the amount remaining after the tax)
     /// @return Actual amount of `j` received
-    function exchangeMIC(uint128 j, uint256 dx, uint256 min_dy) external returns (uint256) {
+    function exchangeMIC(int128 j, uint256 dx, uint256 min_dy) external returns (uint256) {
         //Transfer tokens from the caller
         IERC20(cash).safeTransferFrom(msg.sender, address(this), dx);
 
@@ -59,14 +58,6 @@ contract ProxyCurve is Operator {
             ICurveMeta2(pool).exchange_underlying(0, j, dx, min_dy, msg.sender);
         }
     }
-
-    //function exchange_underlying(uint128 i, uint128 j, uint256 dx, uint256 min_dy, address _receiver) external returns (uint256) {
-
-    //Give the Curve depositer infinite spending privileges, unless we want to spend gas checking the amount of tokens each time someone deposits
-    //No tokens actually remain within this contract after a transaction
-    //function approveForever(address tokenAddress) public onlyOperator {
-    //    IERC20(tokenAddress).approve(pool, 115792089237316195423570985008687907853269984665640564039457584007913129639935);
-    //}
 
     function setFeeCheckerAddress(address _address) public onlyOperator {
         feeChecker = _address;
