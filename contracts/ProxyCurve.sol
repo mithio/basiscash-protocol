7eeec7ebc9dc9c072ddc2e891d2b409e2c28ce08
eepdev <78463658+eepdev@users.noreply.github.com>
2021-04-03 15:24:47 -0400

    Update ProxyCurve.sol
    add_liquidity moved to ProxyCurve.sol
    Adding liquidity is now taxed if Cash is below $1
    Infinite approval is now given to the Curve pool in order to reduce gas cost

--- contracts/ProxyCurve.sol
+++ contracts/ProxyCurve.sol
@@ -20,9 +20,43 @@ contract ProxyCurve is Operator {
     constructor(address _pool, address _cash) public {
         pool = _pool;
         cash = _cash;
+
+        //Giving infinite approval to the pool allows me to skip the two safeApprove() calls, which saves 20k gas
+        IERC20(_cash).approve(_pool, 115792089237316195423570985008687907853269984665640564039457584007913129639935);
     }
 
     
+    /// @notice Deposit coins into the pool
+    /// @param _amount of MIC to deposit
+    /// @param _min_mint_amount Minimum amount of LP tokens to mint from the deposit, should be calculated on the website based on `transferToAmount` (the amount remaining after the tax)
+    /// @return Amount of LP tokens received by depositing (after taxes if applicable)
+    function add_liquidity(uint256 _amount, uint256 _min_mint_amount) external returns (uint256) {
+        //Transfer MIC from caller to contract 
+        IERC20(cash).safeTransferFrom(msg.sender, address(this), _amount);
+
+        if(IFeeChecker(feeChecker).isTransferTaxed2()) {
+            //Calculate tax
+            uint256 feeAmount = IFeeChecker(feeChecker).calculateFeeAmount(address(this), pool, _amount);
+            uint256 transferToAmount = _amount.sub(feeAmount);
+
+            //Send to fee distributor
+            //Note: addFee can only be called by the Cash token, I need to allow FeeDistributor to have a whitelist instead
+            IERC20(cash).safeTransfer(feeDistributor, feeAmount);
+            IFeeDistributor(feeDistributor).addFee(feeAmount);
+
+            //Approve Curve metapool and exchange the amount remaining after the tax
+            //IERC20(cash).safeApprove(pool, 0);
+            //IERC20(cash).safeApprove(pool, transferToAmount);
+
+            //Add liquidity to pool (add_liquidity sends LP tokens to address provided as final argument)
+            return ICurveMeta2(pool).add_liquidity([transferToAmount, 0], _min_mint_amount, msg.sender);
+        }
+        else {
+            //To save gas, the website should use the metapool contract itself when transactions aren't taxed
+            ICurveMeta2(pool).add_liquidity([_amount, 0], _min_mint_amount, msg.sender);
+        }
+    }
+
     /// @notice Perform an exchange from MIC to one of the 3CRV tokens (frontend should only use this contract when selling MIC, otherwise use the metapool contract itself)
     /// @dev Index values can be found via the `underlying_coins` public getter method
     /// @param j Index value of the underlying coin to recieve
@@ -39,12 +73,13 @@ contract ProxyCurve is Operator {
             uint256 transferToAmount = dx.sub(feeAmount);
 
             //Send to fee distributor
+            //Note: addFee can only be called by the Cash token, I need to allow FeeDistributor to have a whitelist instead
             IERC20(cash).safeTransfer(feeDistributor, feeAmount);
             IFeeDistributor(feeDistributor).addFee(feeAmount);
 
             //Approve Curve metapool and exchange the amount remaining after the tax
-            IERC20(cash).safeApprove(pool, 0);
-            IERC20(cash).safeApprove(pool, transferToAmount);
+            //IERC20(cash).safeApprove(pool, 0);
+            //IERC20(cash).safeApprove(pool, transferToAmount);
 
             //For metapools: 0 = the token (i.e. Frax, MIC, etc), 1 = DAI, 2 = USDC, 3 = USDT
             //_receiver = msg.sender, so tokens are sent to the user 
@@ -53,8 +88,9 @@ contract ProxyCurve is Operator {
         else {
             //Transaction isn't taxed, so just do a normal swap
             //To save gas, the website should use the metapool contract when transactions aren't taxed
-            IERC20(cash).safeApprove(pool, 0);
-            IERC20(cash).safeApprove(pool, dx);
+            //IERC20(cash).safeApprove(pool, 0);
+            //IERC20(cash).safeApprove(pool, dx);
+            //function exchange_underlying(int128 i, int128 j, uint256 dx, uint256 min_dy, address _receiver) external returns (uint256);
             ICurveMeta2(pool).exchange_underlying(0, j, dx, min_dy, msg.sender);
         }
     }
